46a3cf4a34d28acf97702ba3f66e0430b1e08b06
Rinkal <rinkaladesara.tops@gmail.com>
2025-05-01 17:45:04 +0530

    var

---
+++ javascript/variable.js
@@ -0,0 +1,26 @@
+a=10
+b=20
+c = a+b
+document.write('<br> Addition is ',c)
+document.write('<br> Add is '+(a+b))
+document.write('<br> Minus is '+(a-b))
+//variable -- vary + able
+//to declare a variable - 4 methods -- 
+//1) Nothing
+//2) var 3) let 4)const
+//var -- redeclare & reassign value
+var i = 25
+document.write('<br> i is '+i)
+var i="Hii"
+document.write('<br> i is '+i)
+//const - constant -- cant redeclare & reassign value
+const pi = 3.14
+var r =2.5
+document.write('<br> Area of circle is '+(pi*r*r))
+// pi = 45
+//let keyword - cant redeclare but reassign is possible
+let str = "Helloo"
+document.write('<br> Str is '+str)
+str = "Hiii"
+document.write('<br> Str is '+str)
+
