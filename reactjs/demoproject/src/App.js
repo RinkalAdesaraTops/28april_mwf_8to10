77a13b72a70375dc598a36531595540a5cf8300f
Rinkal <rinkaladesara.tops@gmail.com>
2025-06-25 09:15:43 +0530

    demo

---
+++ reactjs/demoproject/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstComponent from './FirstComponent';
+function App() {
+  var name="Rinkal"
+  const age = 25
+  let qualification = "MCA"
+  let arr = [10,20,30,40,50]
+  return (
+    <div>
+      {/* <h3>React js Project </h3>
+      <h4>My Name is -- {name}</h4>
+      <h4>Age is -- {age}</h4>
+      <h4>Qualification is - {qualification}</h4>
+      <ul>
+        {
+          arr.map((i,index)=>{
+              return(
+                <li>{i}</li>
+              )
+          })
+        }
+      </ul> */}
+
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjs/demoproject/src/FirstComponent.jsx
@@ -0,0 +1,7 @@
+const FirstComponent = ()=>{
+    return (
+        <h3>First Component Created By me..</h3>
+    )
+}
+
+export default FirstComponent
