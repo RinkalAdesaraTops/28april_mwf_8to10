56402e8ac204e3da720d415f9e525f4aa8d7eeb7
Rinkal <rinkaladesara.tops@gmail.com>
2025-06-30 08:22:20 +0530

    demo435

---
+++ reactjs/demoproject/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// jsx
+function App() {
+  return (
+    <div>
+      <h3>My First React Project created....</h3>
+    {/* virtual DOM */}
+    </div>
+    
+  );
+}
+
+export default App;
